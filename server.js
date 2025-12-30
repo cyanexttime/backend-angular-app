1651b8eb8aa248a1f2df682fc193e88718643bae
Randy <hyperdrive098@gmail.com>
2025-12-30 15:31:42 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend 🚀' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
+
